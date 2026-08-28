646bc553bce3a8054308e10137d9e75553c37005
Steven Li <listeven6691@gmail.com>
2026-08-28 00:55:22 -0400

    Updates

--- assets/swimlanesDiagram-ULZ7WXOC-oehj5jT6.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-UKHOOZJN-D8HRB1Ps.js";import{_ as a}from"./MarkdownRenderer-C2kZlhFq.js";import"./chunk-5VM5RSS4-BSddD54u.js";import"./chunk-XXDRQBXY-DzsgifRm.js";import"./chunk-KBJHAD2P-Dq_Og17A.js";import"./chunk-2GRJ4B5K-DS-lZv1U.js";import"./channel-BN4r74vT.js";import"./index-BMeuoRDQ.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
